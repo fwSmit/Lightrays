db3b4f0ae6a9274de1e82baa051df4b2662884d8
fwSmit <fw.smit01@gmail.com>
2017-10-19 22:13:28 +0200

    Start on materials

---
+++ CircleMaterial.cpp
@@ -0,0 +1,16 @@
+#include "CircleMaterial.h"
+
+CircleMaterial::CircleMaterial()
+{
+    //ctor
+}
+
+CircleMaterial::~CircleMaterial()
+{
+    //dtor
+}
+
+bool CircleMaterial::collide(const class Lightray& ray, class Hitresult& hitresult, bool debugDraw)
+{
+
+}
